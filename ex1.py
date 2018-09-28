53473973b00e1830d2e736c1b769bb6fbfe3f187
Rafik Miah <s184286@users.noreply.github.com>
2018-09-28 15:37:39 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+print('This is exciting.')
+print("Isn't this a thrill?")
+print("I said 'isn't this a thrill?''")
+print('No, you are right, it is "boring".')
---
+++ ex4.py
@@ -0,0 +1,17 @@
+my_name = 'Rafik Miah'
+my_age = 36
+my_height = 64
+my_weight = 112
+my_eyes = 'Brown'
+my_hair = 'Black'
+is_heavy = my_weight > 212
+
+print(f"Let's talk about {my_name}.")
+
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
