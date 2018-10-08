9e3944609380b06e6390a7100723dffe5414113c
Rafik Miah <s184286@users.noreply.github.com>
2018-10-08 20:39:44 +0100

    Add files via upload
    folder added > myPortfolio.
    
    sub folders:
    1) intro to Programming
    2) intro to Web design

---
+++ myPortfolio/intro to Programming/ex5.py
@@ -0,0 +1,13 @@
+A=99;
+x=1;
+y=A-x;
+z=0;
+
+
+if z<A:
+    print(A, "bottles of beer on the wall,")
+    print(A, "bottles of beer.")
+    print("Take one down, pass it around,")
+    print(y, "bottles of beer on the wall…")
+else:
+    print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall…")
