2c89584f3b99e4f7396cadf1e5b50e5acdfa2983
Rafik Miah <s184286@users.noreply.github.com>
2018-10-11 11:11:45 +0100

    Add files via upload
    week 3 task ex8.py uploaded

---
+++ myPortfolio/intro to Programming/ex8.py
@@ -0,0 +1,22 @@
+"""
+
+Task 1
+
+Last week, we somewhat unrealistically assumed that while saving for a house you would never earn
+a raise. That seems harsh, so let’s go back and fix that by saying that we’ll get a raise every 6 months.
+
+Open ex7.py and save a new copy as ex8.py. Modify the code in the following ways:
+
+1. Have the user input a semi-annual salary raise: semi_annual_raise (as a decimal percentage)
+2. After the 6th month, increase your salary by that percentage. Do the same every 6 months
+after that (12th, 18th etc).
+
+Your output should be identical to the last attempt except for the addition of the new user input.
+Be sure to commit each attempt at this problem to the portfolio repository, not just your final piece
+of code.
+
+git .
+git commit -m “useful commit message”
+git push
+
+"""
