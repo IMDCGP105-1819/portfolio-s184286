da25d64fce25e985dcfe38b918e6f2a7906b78ff
Rafik Miah <s184286@users.noreply.github.com>
2018-10-15 19:28:14 +0100

    Add files via upload
    while loop - ex5.py

--- myPortfolio/intro to Programming/ex5.py
+++ myPortfolio/intro to Programming/ex5.py
@@ -1,13 +1,7 @@
-A=99;
-x=1;
-y=A-x;
-z=0;
 
 
-if z<A:
-    print(A, "bottles of beer on the wall,")
-    print(A, "bottles of beer.")
-    print("Take one down, pass it around,")
-    print(y, "bottles of beer on the wall…")
-else:
-    print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall…")
+n = 99
+while n > 0:
+    print(n,"bottles of beer on the wall,","Take one down, pass it around,")
+    n = n - 1
+print('No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall…')
