7d2df2efb4e4f505ad7ad98ad625ed717edd0472
Rafik Miah <s184286@users.noreply.github.com>
2018-10-17 12:10:48 +0100

    Update ex8.py
    task incomplete. requires editing/modification and new user input.

--- myPortfolio/intro to Programming/ex8.py
+++ myPortfolio/intro to Programming/ex8.py
@@ -17,6 +17,33 @@ of code.
 
 git .
 git commit -m “useful commit message”
-git push
+git push
+
 
 """
+## House deposit save time calculator
+DEPOSIT_RATE = 0.2
+SAVINGS_INTEREST_RATE = 0.04
+
+# Get required data from user
+salary = float(input("Enter your annual salary:"))
+total_cost = float(input("Enter the cost of your dream home:"))
+saved_portion = float(input("Enter the percent of your salary to save, as a decimal:")) / 100
+
+# Calculate required values
+savings_target = total_cost * DEPOSIT_RATE
+monthly_savings = salary * saved_portion / 12
+
+# Track savings over time
+current_savings = monthly_savings
+total_time = 0
+
+# Increment time until target savings reached
+while(current_savings < savings_target):
+    current_savings += (current_savings * SAVINGS_INTEREST_RATE) + monthly_savings
+    total_time += 1
+
+# Display results
+print(f"Deposit: £{savings_target: .2f}")
+print(f"Monthly Savings: £{monthly_savings: .2f}")
+print(f"Time: {total_time} months")
