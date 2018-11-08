174f79016778e22ae526b5c4e53c52b473b1e6d0
Rafik Miah <s184286@users.noreply.github.com>
2018-11-08 22:06:23 +0000

    Update ex2.py

--- myPortfolio/intro to Programming/ex2.py
+++ myPortfolio/intro to Programming/ex2.py
@@ -9,7 +9,7 @@ print("Now I will count my plants:")
 
 print(3+2+1-5+4%2-1/4+6)                                # print(sum)
 
-print("Is it true that 3+2<5-7?")                       # check with operator < less-than
+print("Is it true that 3+2<5-7?")                       # operator < less-than
 print(3+2<5-7)
 
 print("What is 3+2?",3+2)
@@ -17,9 +17,9 @@ print("What is 5-7?",5-7)
 
 print("Can you see why that printed False earlier?")
 
-print("Is it greater?",5>-2)                            # check with operator > greater  than
-print("Is it greater or equal?", 5>=-2)                 # check with operator > greater than or equal to
-print("Is it less or equal?",5<=-2)                     # check with operator <= less than or equal to  
+print("Is it greater?",5>-2)                            # operator > greater  than
+print("Is it greater or equal?", 5>=-2)                 # operator > greater than or equal to
+print("Is it less or equal?",5<=-2)                     # operator <= less than or equal to  
 
 
 
