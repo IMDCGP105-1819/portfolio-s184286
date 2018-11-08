aa0301d50a67fcd946a7009df7bccc0859082239
Rafik Miah <s184286@users.noreply.github.com>
2018-11-08 22:48:13 +0000

    Update ex6.py

--- myPortfolio/intro to Programming/ex6.py
+++ myPortfolio/intro to Programming/ex6.py
@@ -1,26 +1,4 @@
-"""
-# Task
-Read about Pythons built in “input” function:
-https://docs.python.org/3/library/functions.html#input
-NB
-: The behaviour of input has changed significantly between Python 2.x and 3.x, so be sure that
-you are looking at information for the correct version.
-Using this, write a program that asks the user to fill out the information you printed in ex4.py from
-the last worksheet.
 
-Name
-Age
-Height (specify inches or cm)
-Weight (specify pounds of kilograms)
-Eye colour
-Hair colour
-
-Use if statements to print different outputs for age, height and weight depending on if they are
-above or below specific thresholds. Be polite!
-
-Save the work as ex6.py, make regular commits to GitHub
-"""
-print ("Exercise ex6.py - intro to programming\n")
 
 name = input("Enter your name: ")
 print("Your name is", name, ".")
