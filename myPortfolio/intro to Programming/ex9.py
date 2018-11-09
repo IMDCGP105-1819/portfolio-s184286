c050923b76bbabe2210af765574d3385a160438a
Rafik Miah <s184286@users.noreply.github.com>
2018-11-09 01:54:57 +0000

    Update ex9.py
    attempt at ex9.py , implementing bisection search, >> not functioning :( 
    
    requires rewrite an updating.

--- myPortfolio/intro to Programming/ex9.py
+++ myPortfolio/intro to Programming/ex9.py
@@ -1,75 +1,57 @@
-"""
 
-Task 2
-
-Let’s re-work how we approach the saving goal; rather than saying how long it might take us to save
-up a specific value, let’s ask how much we need to save to achieve a specific target goal in a given
-time period.
-
-To simply things a little, we can make the following assumptions:
-1. Your semi-annual raise is 0.07 (7%).
-2. Your investments have an annual rate of return of 0.04 (4%).
-3. The down payment is 0.25 (25%) of the cost of the house.
-4. The cost of the house you are saving for is £1M.
-
-You are now going to attempt to find the best rate of savings to achieve a down payment to a
-million-pound house in 36 months. Rather than trying to hit that value exactly, there is a £100 leeway
-either side of the deposit value.
-In ex9.py, write a program to calculate the best savings rate, as a function of your starting salary. You
-should use a bisection search to do this efficiently. You should keep track of the number of steps
-required for your search to finish. You should be able to reuse some existing code to help solve this
-problem.
-
-Because we are searching for a value that is in principle a float (the best rate of savings) we are going
-to limit ourselves to two decimal places – we may want to save at 7.04%, but we are not going to
-worry about the difference between 7.041% and 7.039%. This means we can search for an integer
-between 0 and 10000 and then convert it to a decimal percentage to use when we are calculating 
-the current_savings after 36 months. This range allows us to avoid a possible infinite loop that could
-arise were we searching the infinite number of decimals between 0 and 1.
-Your program should allow you to enter different values for the starting salary and it should account
-for the fact that some salaries will not allow you to save the required amount within 3 years.
-
-Hints
-    • There may be multiple savings rates that yield a savings amount that is within £100 of the required deposit. In this case, you can just return any of the possible values.
-    • Depending on your stopping condition and how you compute a trial value for bisection search, your number of steps may vary from the example below.
-    • Watch out for integer division when calculating if a percentage saved is appropriate and when calculating the final decimal percentages rate.
-    • Remember to reset the appropriate value(s) to their initial values for each iteration of the bisection search.
-    
-Examples
-
-Enter the starting salary: 150000
-Best savings rate: 0.4411
-Steps in bisection search: 12
-Enter the starting salary: 10000
-It is not possible to pay the down payment in three years
-
-"""
-
-#input
-annual_salary=float(input("Enter the starting salary:"))
-print("Best savings rate:")
-print("Steps in bisection search:")
-print("the cost of the house you are saving for is £1M.")
+	
+# write a program to calculate the best savings rate, as a function of your starting salary
+# bisection search.
 
 
-# portion_saved=float(input("Enter the percent of your salary to save, as a decimal: ")) 
+annual_salary = float(input("Enter annual salary amount: "))
 
-
-# values
-
-total_cost=float(1000000)
-portion_deposit = 0.20 
-monthly_salary = ((annual_salary/12)+(semi_annual_raise/6))                                                            
-deposit = (total_cost/portion_deposit)                                                                      
+total_cost=1000000
+semi_annual_raise=0.07
+monthly_salary = ((annual_salary/12)+(semi_annual_raise/100*6))                                 
+down_payment= (total_cost*0.25)                                                                      
 current_savings=0                                                                            
-r = 0.4
+r = 0.04
+
 months_required =0
 interest = (current_savings*r/12)
-semi_annual_raise = annual_salary *0.07
+months_to_save=36
 
-#sum
 
-while(current_savings < deposit):
-    current_savings+=(monthly_salary+interest)
-    months_required+=1
-print("Number of months: " , months_required)
+
+low=0
+high=100000
+epsilon = 100
+guess= (high + low)/2.0
+steps = 0
+
+
+
+while abs(current_savings <= down_payment + epsilon) and abs(current_savings >= down_payment - epsilon):
+	for i in range(1, months_to_save+1):
+		current_savings +=monthly_salary
+		current_savings +=(interest/12)
+		months +=1
+		if month % 6==0:
+			current_savings += (current_savings*semi_annual_raise)
+		months +=1
+	if current_savings < down_payment + epsilon or current_savings < down_payment - epsilon:
+		print("..not possible for you to afford the deposit within the time period")
+		break
+	current_savings =0
+	months=0
+	for i in range(1, months_to_save+1):
+		current_savings += (monthly_salary * guess)
+		current_savings += (current_savings * interest/12)
+		months+=1
+		if months % 6==0:
+			current_savings += (current_savings *semi_annual_return)
+	if current_savings < down_payment:
+		low = guess
+	else:
+		high = guess
+		guess = guess/10000
+		steps +=1
+		print("Best savings rate: ", round(guess,2))
+		print("Steps in bisection:", steps)
+		
