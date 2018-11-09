1be58c4291f49e69c6d2e651a0df9232ed7c3571
Rafik Miah <s184286@users.noreply.github.com>
2018-11-09 02:32:15 +0000

    Update ex11.py
    attempt at ex11.py, requires further revision. incorrect outputs received.

--- myPortfolio/intro to Programming/ex11.py
+++ myPortfolio/intro to Programming/ex11.py
@@ -1,18 +1,27 @@
-"""
-Task 2
 
-Python can generate random numbers using a function provided by the random library. At the start
-of your file, place the following line of code:
 
-from random import randomint
 
-This gives us access to the randomint function – which takes two integer arguments; low and high. It
-returns a random value between (and including) those two limits.
+# Write a function that selects a random number and then asks the user to guess what that number is. 
+# They should be told if they are higher or lower than the result, track the number of guesses they take to get the correct value. 
+# If they do not guess correctly, ask them again until they do.
 
-Write a function that selects a random number and then asks the user to guess what that number is.
-They should be told if they are higher or lower than the result, track the number of guesses they
-take to get the correct value. If they do not guess correctly, ask them again until they do.
 
-Save your code as ex11.py.
 
-"""
+
+from random import randint
+print(randint(0, 100))	
+		
+		
+def my_function():
+  print("try guess the number..")
+  
+if randint == 0: 
+	print("answer is to low")
+	print("guess again..")
+elif randint == 100:
+	print("answer is to high")
+	print("guess again..")
+else:
+	print("answer is correct")
+
+my_function()	
