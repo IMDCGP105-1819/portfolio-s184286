1542a624631d7e24ff2c48de15b38678063bd0b2
Rafik Miah <s184286@users.noreply.github.com>
2018-11-11 00:45:35 +0000

    Add files via upload
    task incomplete

---
+++ myPortfolio/intro to Programming/dictionary.py
@@ -0,0 +1,43 @@
+
+# a frequency dictionary that maps string:int key value pair
+
+Eds_song=str("See, Im true, my songs are where my heart is Im like glue, I stick to other artists Im not you, now that would be disastrous Let me sing and do my thing and move to greener pastures See, I'm real, I do it all, it's all me Im not fake, dont ever call me lazy I wont stay put, give me the chance to be free Suffolk sadly seems to sort of suffocate me")
+
+list_1 = ["Now" "I'm" "in" "town""," "break" "it" "down""," "thinking" "of" "making" "a" "new" "sound"
+"Playing" "a" "different" "show" "every" "night" "in" "front" "of" "a" "new" "crowd"
+"That's" "you" "now""," "ciao""," "seems" "that" "life" "is" "great" "now"
+"See" "me" "lose" "focus""," "as" "I" "sing" "to" "you" "loud"
+"And" "I" "can't""," "no""," "I" "won't" "hush"
+"I'll" "say" "the" "words" "that" "make" "you" "blush"
+"I'm" "gonna" "sing" "this" "now", "", ""]
+
+dictionary_1 =	{
+  "Eds_song": "1",
+  "abcdefghijklmnopqrstuvwxyz": "2",
+  "0123456789":"3" 
+}
+
+print("DICTIONARY OUTPUT, STRINGS: INT KEY VALUE ", dictionary_1)
+print("THE LENGTH IS: ", len(dictionary_1))
+print(len(Eds_song))
+
+print(list_1)
+list_1.append("you")
+list_1.append("need")
+list_1.append("me")
+list_1.append("man")
+
+
+
+
+"""
+Cause you need me, man, I dont need you
+You need me man, I dont need you
+You need me man, I dont need you at all
+You need me man, I dont need you
+
+You need me man, I dont need you
+You need me man, I dont need you
+You need me man, I dont need you at all
+You need me"")
+"""
