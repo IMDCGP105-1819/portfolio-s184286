7f0305702b2297f17d0302ab731f820303a56479
Rafik Miah <s184286@users.noreply.github.com>
2018-12-04 16:33:18 +0000

    first draft of poker_game

---
+++ poker_game.py
@@ -0,0 +1,81 @@
+
+"""
+Consider the game of poker (the variant does not matter), 
+break down the elements into objects that you could represent with classes. 
+As a starting point think about:  
+a card, the deck of cards, a hand of cards (or the player who holds the hand). 
+
+How would you represent each of those objects? 
+What are their attributes? 
+What data do they need to hold? What methods? 
+How might you sensibly represent the data?
+Write code to represent each object as a class, 
+build a small application that will “deal” a hand of 5 cards objects. 
+It should print each hand to the console in a sensible format and then allow another hand to be dealt – continuing through until there are not enough cards left in the deck.
+The deck should be shuffled at the start of the process (and again if all cards are dealt out). 
+Consider how you might shuffle the deck, what approaches might you take to organise your list of cards into a random order?
+Your code should be commented to explain your thinking about each step of the process, write some test functions that demonstrate that your classes work as expected.
+
+"""
+
+
+
+
+
+print ("Welcome to my Texas Hold'em Poker Game")
+
+input("would you like to begin...")
+
+
+
+
+# classes
+
+# dealer
+# deck of cards
+# shuffle
+# players
+# player hand
+# moves
+# 5 card draw
+# wining hand
+
+# Deck:
+1=
+2=
+3=
+4=
+5=
+6=
+7=
+8=
+9=
+10=
+J=
+Q=
+K=
+A=
+
+# draw
+
+1- Royal Flush = A K Q J 10
+2- Straight Flush = 10 9 8 7 6
+3- Four of a Kind = Q Q Q Q 
+4- Full House = A A A K K 
+5- Flush = K 8 6 4 2
+6- Straight = 8 7 6 5 4
+7- Three of a Kind = Q Q Q 
+8- Two pair = J J 9 9
+9- One pair = 
+10- High card = A Q 6 4 2
+
+# wining hand
+
+if player_hand == draw
+print player x wins
+
+class DeckOfCards(object):
+    def __init__(self,name):
+        self.name = ""
+
+    
