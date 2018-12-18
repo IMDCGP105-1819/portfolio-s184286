c3ef093d8de430b5918e024700cdada38f1fc2e5
Rafik Miah <s184286@users.noreply.github.com>
2018-12-18 23:13:28 +0000

    update to poker_game in py.

--- poker_game.py
+++ poker_game.py
@@ -1,44 +1,76 @@
 
+    
+print(r"""
+__________         __________       __                     _____              .___  .___                           
+\______   \___.__. \______   \____ |  | __ ___________    /     \ _____     __| _/__| _/____   ____   ______ ______
+ |     ___<   |  |  |     ___/  _ \|  |/ // __ \_  __ \  /  \ /  \\__  \   / __ |/ __ |/    \_/ __ \ /  ___//  ___/
+ |    |    \___  |  |    |  (  <_> )    <\  ___/|  | \/ /    Y    \/ __ \_/ /_/ / /_/ |   |  \  ___/ \___ \ \___ \ 
+ |____|    / ____|  |____|   \____/|__|_ \\___  >__|    \____|__  (____  /\____ \____ |___|  /\___  >____  >____  >
+           \/                           \/    \/                \/     \/      \/    \/    \/     \/     \/     \/ 
+ 
+""")
+
+
+
+
+print("would you like to begin...")
+input("press enter..")
+
+player1_name = input ("enter name for player 1: ")
+player2_name = input ("enter your name player 2: ")
+
+
+print(r"""
+          _____
+         |A .  | _____
+         | /.\ ||A ^  | _____
+         |(_._)|| / \ ||A _  | _____
+         |  |  || \ / || ( ) ||A_ _ |
+         |____V||  .  ||(_'_)||( v )|
+                |____V||  |  || \ / |
+                       |____V||  .  |
+                              |____V|
+
+""")
+
+
+print("ok.. " + player1_name + " vs " + player2_name)
+
 """
-Consider the game of poker (the variant does not matter), 
-break down the elements into objects that you could represent with classes. 
-As a starting point think about:  
-a card, the deck of cards, a hand of cards (or the player who holds the hand). 
+def play()
 
-How would you represent each of those objects? 
-What are their attributes? 
-What data do they need to hold? What methods? 
-How might you sensibly represent the data?
-Write code to represent each object as a class, 
-build a small application that will “deal” a hand of 5 cards objects. 
-It should print each hand to the console in a sensible format and then allow another hand to be dealt – continuing through until there are not enough cards left in the deck.
-The deck should be shuffled at the start of the process (and again if all cards are dealt out). 
-Consider how you might shuffle the deck, what approaches might you take to organise your list of cards into a random order?
-Your code should be commented to explain your thinking about each step of the process, write some test functions that demonstrate that your classes work as expected.
+while 
 
-"""
+if
+
+else:
 
 
 
 
+class Cards(object):
+    def __init_ (self, name, suit, number):
+        self.name = name
+        self.suit = suit
+        self.number = number
+    
+    def Card_number(self):
+        return self.name + self.number
+    
+    def Card_suit(self):
+        if suit_1 == true:
+            print("Heart")
+        if suit_2 == true:
+            print("Spade")
+        if suit_3 == true:
+            print("Diamond")
+        if suit_4 == true:
+            print("Club")
 
-print ("Welcome to my Texas Hold'em Poker Game")
-
-input("would you like to begin...")
 
 
+# classes: dealer / deck of cards / shuffle /players / player hand /moves / 5 card draw /  wining hand...
 
-
-# classes
-
-# dealer
-# deck of cards
-# shuffle
-# players
-# player hand
-# moves
-# 5 card draw
-# wining hand
 
 # Deck:
 1=
@@ -71,11 +103,10 @@ A=
 
 # wining hand
 
-if player_hand == draw
+if player_ == ?
 print player x wins
 
-class DeckOfCards(object):
-    def __init__(self,name):
-        self.name = ""
 
-    
+"""
+
+   
